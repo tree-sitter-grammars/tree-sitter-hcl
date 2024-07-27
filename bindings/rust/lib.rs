abfc6392d640803269f786dd19ea1a48856fd6f1
Alex Povel <git@alexpovel.de>
2024-07-27 10:37:46 +0200

    chore: Regenerate updates to Rust bindings
    This is the result of running
    
    ```bash
    rm bindings/rust/{lib,build}.rs && cargo install tree-sitter-cli@0.22.6 && ~/.cargo/bin/tree-sitter generate
    ```
    
    and committing only the Rust changes. Removing
    bindings first gives a clean slate with slightly
    different results.

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,13 +1,15 @@
-//! This crate provides hcl language support for the [tree-sitter][] parsing library.
+//! This crate provides Hcl language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
-//! let code = "";
+//! let code = r#"
+//! "#;
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(&tree_sitter_hcl::language()).expect("Error loading hcl grammar");
+//! parser.set_language(&tree_sitter_hcl::language()).expect("Error loading Hcl grammar");
 //! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
@@ -35,10 +37,10 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 // Uncomment these to include any queries that this grammar contains
 
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+// pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+// pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+// pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -47,6 +49,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(&super::language())
-            .expect("Error loading hcl language");
+            .expect("Error loading Hcl grammar");
     }
 }
