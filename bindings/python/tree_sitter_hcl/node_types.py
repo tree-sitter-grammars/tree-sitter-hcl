72f35dac72862064a13d6b39a14414aac4eea63a
Alex Sweet <asweet@confluent.io>
2025-06-11 02:19:15 -0700

    Add generated types

---
+++ bindings/python/tree_sitter_hcl/node_types.py
@@ -0,0 +1,11 @@
+from tree_sitter import Language, Node, Parser, Tree, TreeCursor
+
+
+HclNode = Node
+HclTree = Tree
+HclTreeCursor = TreeCursor
+HclLanguage = Language
+HclParser = Parser
+
+Expression = None
+Identifier = None
